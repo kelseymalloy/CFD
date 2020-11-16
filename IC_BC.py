6d8060f9e05a5432b6a3ee763bfd2ab3947fefd2
kelsey malloy <kelseymalloy4@gmail.com>
2020-11-16 14:32:14 -0500

    update

--- IC_BC.py
+++ IC_BC.py
@@ -4,17 +4,22 @@ def gauss(X,Y,nx,ny,amp,radius):
     # returns gaussian bubble
     return amp*(np.exp(-((X-nx/2)**2/(2*(radius)**2) + (Y-ny/2)**2/(2*(radius)**2))))
 
-def BC(u,v,wtype):
-    if wtype == 0: # no slip boundary
-        u[:, -2:] = 0
-        #u[:, :2] = 0
 
-        #v[:2,:] = 0
-        v[-2:,:] = 0
+def wave(X,Y,alpha,m,n,a,b):
+   return alpha*(np.cos(m*np.pi*X/a))*(np.cos(n*np.pi*Y/b))
 
-    if wtype == 1: # periodicity
-        u[:, -2:] = u[:,1]
-        v[-2:,:] = v[:,1]       
+
+def BC(u,v):
+    #if wtype == 0: # no slip boundary
+    u[:, 0] = 0
+    u[:, -1] = 0
+
+    v[0,:] = 0
+    v[-1,:] = 0
+
+    #if wtype == 1: # periodicity
+    #    u[:, -2:] = u[:,1]
+    #    v[-2:,:] = v[:,1]       
 
         #v[0,:] = 0
         #v[-1,:] = 0
--- SWE_RHS.py
+++ SWE_RHS.py
@@ -10,16 +10,22 @@ def SWE_RHS(p,u,v,f,depth,g,dx,dy):
     h = depth + p
     
     # compute x-average of h on u-points
-    havg_x = avg_x(h)
+    havg_x = np.zeros(ru.shape)
+    havg_x[:,1:-1] = avg_x(h)
+    havg_x[:,0] = h[:,0]
+    havg_x[:,-1] = h[:,-1]
     
     # compute x-mass flux U=hbx*u on u-points
-    U = havg_x * u[:,1:-1]
+    U = havg_x * u
 
     # compute y-average of h on v-points, hby
-    havg_y = avg_y(h)
+    havg_y = np.zeros(rv.shape)
+    havg_y[1:-1,:] = avg_y(h)
+    havg_y[0,:] = h[0,:]
+    havg_y[-1,:] = h[-1,:]
     
     # compute y-mass flux V=hby*v on v-points    
-    V = havg_y * v[1:-1,:]
+    V = havg_y * v
     
     # compute the x-gradient of (U)_x=gradx(U)
     dU_dx = d_dx(U,dx)
@@ -27,7 +33,7 @@ def SWE_RHS(p,u,v,f,depth,g,dx,dy):
     # add-in the y-gradient of (V)_y=grady(V)
     dV_dy = d_dy(V,dy)
 
-    rp[1:-1,1:-1] = -dU_dx[1:-1,:]-dV_dy[:,1:-1]
+    rp = -dU_dx-dV_dy
 
     # pressure is tp = g*p + 0.5* ( avx(u^2)+avy(v^2) )
     tp = g*p + 0.5*(avg_x(u**2)+avg_y(v**2))
@@ -35,30 +41,27 @@ def SWE_RHS(p,u,v,f,depth,g,dx,dy):
     # x-gradient of tp and add it to ru
     dtp_dx = d_dx(tp,dx)  
     ru[:,1:-1] = -dtp_dx
-    
-    
+     
     # y-gradient of tp and add it to rv
     dtp_dy = d_dy(tp,dy)
     rv[1:-1,:] = -dtp_dy
     
     # Coriolis term
     # compute q = (v_x - u_y + f)/avx(avy(h))
-    dv_dx = d_dx(v,dx)
-    du_dy = d_dy(u,dy)
-    q = (dv_dx[1:-1,:] - du_dy[:,1:-1] + f[1:-1,1:-1])/(avg_x(avg_y(h)))
+    #dv_dx = d_dx(v,dx)
+    #du_dy = d_dy(u,dy)
+    #q = (dv_dx[1:-1,:] - du_dy[:,1:-1] + f[1:-1,1:-1])/(avg_x(avg_y(h)))
     
     # compute avx(V) and avy(U) on z-points
-    Vavg_x = avg_x(V)
-    Uavg_y = avg_y(U)
+    #Vavg_x = avg_x(V)
+    #Uavg_y = avg_y(U)
     
     # compute avy(q*V) on u-points and add it to ru
-    qVavg_y = avg_y(q*Vavg_x)
-    ru[1:-1,1:-1] = qVavg_y
+    #qVavg_y = avg_y(q*Vavg_x)
+    #ru[1:-1,1:-1] = ru[1:-1,1:-1]+qVavg_y
 
     # compute avx(q*U) on v-points and add it to rv
-    qUavg_x = avg_x(q*Uavg_y)
-    rv[1:-1,1:-1] = -qUavg_x
+    #qUavg_x = avg_x(q*Uavg_y)
+    #rv[1:-1,1:-1] = rv[1:-1,1:-1]-qUavg_x
     
-    #print(rp.shape,ru.shape,rv.shape)
-
     return rp,ru,rv
